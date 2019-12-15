0d4d2b020a4e3a67715588476f6be16cd838b684
tianqian <tianqian@ada.sina.com.cn>
2019-12-15 14:03:59 +0800

    初始化

---
+++ src/pages/demo/Life.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import './index.less'
+export default class Life extends React.Component{
+  render() {
+    return (
+      <div className="content">child</div>
+    )
+  }
+}
